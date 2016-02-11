87815e3fa54432de6417e322503726980820c531
Daniel Pipkin <dpip.nc@gmail.com>
2016-02-11 11:01:42 -0500

    yip

---
+++ Kung-Fury.js
@@ -0,0 +1,12 @@
+var kungFury = [
+    { title: "Kung Fury ",
+      duration: 34 + "minutes.",
+      stars:"Hitler, Triceracop, Zeus, Fury."
+
+    }
+    ];
+
+    for (var i = 0; i < kungFury.length; i ++) {
+        console.log(kungFury[i].title + "lasts for about " + kungFury[i].duration + kungFury[i].stars)
+    }
+    
