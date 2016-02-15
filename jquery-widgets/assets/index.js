ce1fd72de61881214bb14f46760b940442ab7afd
Daniel Pipkin <dpip.nc@gmail.com>
2016-02-15 17:06:44 -0500

    closer

--- jquery-widgets/assets/index.js
+++ jquery-widgets/assets/index.js
@@ -1,6 +1,26 @@
-$( "#sweet" ).on("click", function () {
-      alert($(this).text());
+// $( "#sweet" ).on("click", function () {
+//       alert($(this).text());
+//
+//       $( "#sweet" ).fadeIn( "slow", function() {
+//       });
+//     });
 
-      $( "#sweet" ).fadeIn( "slow", function() {
-      });
-    });
+(function () {
+$(".container1").on("click", function() {
+  event.preventDefault();
+  $("#overlay")
+      .fadeIn()
+      .find("#modal")
+      .fadeIn();
+
+});
+
+$(".close").on("click", function() {
+  event.preventDefault();
+  $("#overlay")
+      .fadeIn()
+      .find("#modal")
+      .fadeIn();
+
+});
+})();
