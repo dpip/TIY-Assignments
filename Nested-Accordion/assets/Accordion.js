a03f21268fcff153cfce1ef3554a852566b82f74
Daniel Pipkin <dpip.nc@gmail.com>
2016-02-17 01:07:18 -0500

    Query edits

--- Nested-Accordion/assets/Accordion.js
+++ Nested-Accordion/assets/Accordion.js
@@ -1,4 +1,25 @@
- var openUp = document.getElementById("Arrow")
- openUP.addEventListener( "click", function () {
-   alert("Hello Pip");
- }, false;
+$(document).ready(function() {
+    $("#arrowHead").click(function(){
+    $(".accordiOne").slideToggle();
+  })
+})
+$(document).ready(function() {
+    $("#arrowOne").click(function(){
+    $(".accordiTwo").slideToggle();
+  })
+})
+$(document).ready(function() {
+    $("#arrowTwo").click(function(){
+    $(".accordiThree").slideToggle();
+  })
+})
+$(document).ready(function() {
+    $("#arrowThree").click(function(){
+    $(".accordiFour").slideToggle();
+  })
+})
+$(document).ready(function() {
+    $("#arrowFour").click(function(){
+    $(".accordiFive").slideToggle();
+  })
+})
