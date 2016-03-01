4e932b2790b118319cab9a0a84a4cd7959bf9a83
Daniel Pipkin <dpip.nc@gmail.com>
2016-03-01 10:43:40 -0500

    1

---
+++ tag-function.js
@@ -0,0 +1,8 @@
+var tags = ['laser', 'clothing', 'hash'];
+var numberOf = tags.slice(0).sort().indexOf();
+
+    for(i=0; i <=2; i++) {
+    tags[i] = tags[i].length;
+    }
+
+console.log(numberOf);
