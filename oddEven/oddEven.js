45c6835913c33139b038d0854a48221b5ad58d03
Daniel Pipkin <dpip.nc@gmail.com>
2016-03-02 10:37:23 -0500

    up

---
+++ oddEven/oddEven.js
@@ -0,0 +1,12 @@
+var binNum = [21044];
+
+var check = function() {
+    for(i = 0; i < 21044; i++) {
+     var checkOut = [i];
+     console.log([i]+1);
+    }
+};
+
+// $.each(binNum function(odds, evens) {
+//     console.log(binNum);
+// });
