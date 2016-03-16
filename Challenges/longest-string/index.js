0c0061e4581d10facf3cc30a9f0fd57449b08ee9
Daniel Pipkin <dpip.nc@gmail.com>
2016-03-16 10:36:14 -0400

    challenge

---
+++ Challenges/longest-string/index.js
@@ -0,0 +1,18 @@
+// Write a JavaScript function that accepts a list of country names as input and returns the longest country name as output.
+
+// sample function : Longest_Country_Name(["Australia, Germany, United States of America"])
+
+// output shoulf be the US
+
+
+var countries = ["Mexico", "Canada", "Nepal", "Argentina"]
+
+function longest() {
+    for(i = 0; i > countries.length; i ++)
+        countries = i[3];
+        console.log(countries[3]);
+
+
+}
+
+longest();
